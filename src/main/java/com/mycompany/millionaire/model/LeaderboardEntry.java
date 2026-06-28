27a9643240a9b42c891eb2225404ff53931d8796
Vương Kiến Hào <109871441+duocornua@users.noreply.github.com>
2026-06-28 19:53:49 +0700

    .

--- src/main/java/com/mycompany/millionaire/model/LeaderboardEntry.java
+++ src/main/java/com/mycompany/millionaire/model/LeaderboardEntry.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.millionaire.model;
 
-/**
- *
- * @author Admin
- */
 public class LeaderboardEntry implements Comparable<LeaderboardEntry> {
 
     private String playerName;
